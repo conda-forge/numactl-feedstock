889a7c986bab7f737d11fff03b19d977e9531bfe
Keith Kraus <keith.j.kraus@gmail.com>
2024-03-14 21:41:03 -0400

    Try to add verbosity to configure
    Co-authored-by: Jacob Wujciak-Jens <jacob@wujciak.de>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@ set -eu
 ./configure \
     --prefix="$PREFIX" \
     --enable-static=no \
-    --enable-shared=yes
+    --enable-shared=yes \
+    --verbose
 
 make V=1
 make check || { cat ./test-suite.log && false; }
